b2be05ddff51bb9d51b71c5cc47e94c65732d06d
ajl2881 <ajl2881@rit.edu>
2024-03-03 08:43:17 -0500

    Added tea quiz info

---
+++ src/data/teaquestionset.tsx
@@ -0,0 +1,22 @@
+// QuestionSet.js
+export const quiz = {
+    questions: [
+        {
+            id: 1,
+            question: 'What caffeine level are you looking for?',
+            answers: ['Caffeine Free', 'Low to Medium', 'High'],
+        },
+
+        {
+            id: 2,
+            question: `What tea do you usually like?`,
+            answers: ['Black', 'Green', 'White', 'Oolong'],
+        },
+        {
+            id: 3,
+            question: 'What flavor note sounds most interesting?',
+            answers: ['Vegetal', 'Fruit', 'Floral','Spice'],
+        },
+    ],
+};
+ 
